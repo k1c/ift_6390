fixing shape index in gradient calculation

homework2/solution.py
@@ -39,7 +39,7 @@ class SVM:
         scores = x.dot(self.w)
         active = (np.multiply(scores, y) < 1).astype(float)
         grad = np.dot(-x.T, np.multiply(y, active))
-        grad = 2 * self.C * grad / y.shape[1]
+        grad = 2 * self.C * grad / y.shape[0]
         # Regularization term
         grad += self.w
         return grad
@@ -118,20 +118,16 @@ if __name__ == "__main__":
     y_train = np.load("train_labels.npy")
     y_test = np.load("test_labels.npy")
 
-    svm = SVM(eta=0.01, C=30, niter=200, batch_size=5000, verbose=True)
+    svm = SVM(eta=0.001, C=30, niter=200, batch_size=5000, verbose=False)
     # to compute the gradient or loss before training, do the following:
     y_train_ova = svm.make_one_versus_all_labels(y_train, 10) # one-versus-all labels
     svm.w = np.zeros([401, 10])
     grad = svm.compute_gradient(x_train, y_train_ova)
-    print(grad.shape)
     loss = svm.compute_loss(x_train, y_train_ova)
-    print(loss)
 
-    print("Fitting the model...")
     train_loss, train_accuracy, test_loss, test_accuracy = svm.fit(x_train, y_train, x_test, y_test)
 
     # to infer after training, do the following:
     y_inferred = svm.infer(x_test)
-    print(y_inferred)
 
 