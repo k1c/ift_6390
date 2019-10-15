9fe5c92f2f0f51669942eac8d2304fcee0b3ff5a
Isabelle Bouchard <isabelle@elementai.com>
2019-10-15 15:24:17 -0400

    added dummy preprocess function

--- kaggle/solution.py
+++ kaggle/solution.py
@@ -10,12 +10,19 @@ import pathlib
 
 DATA_PATH = pathlib.Path("data/")
 
-def read_data(set_="train"):
+def read_data(set_):
     return np.load(DATA_PATH / f"data_{set_}.pkl", allow_pickle=True)
 
+def preprocess(data):
+    return data
+
+def get_data(set_):
+    data = read_data(set_)
+    return preprocess(data)
+
 def main():
-    X_train, y_train = read_data(set_="train")
-    X_test = read_data(set_="test")
+    X_train, y_train = get_data(set_="train")
+    X_test = get_data(set_="test")
 
 if __name__ == "__main__":
     main()
