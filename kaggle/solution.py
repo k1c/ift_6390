e7a476fe2b29b3393055326be2baf18d713668f6
k1c <cpelletier@klipfolio.com>
2019-10-15 22:40:53 -0400

    WIP - does not currently run, set up the structure based off examples provided

--- kaggle/solution.py
+++ kaggle/solution.py
@@ -9,6 +9,11 @@ import random
 import numpy as np
 import pathlib
 from scipy.sparse import csr_matrix
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
 
 DATA_PATH = pathlib.Path("data/")
 
@@ -48,6 +53,7 @@ class NaiveBayesModel:
             indptr.append(len(indices))
         return vocabulary, csr_matrix((data, indices, indptr), dtype=int)
 
+# TODO model.train should take X_val and y_val and return validation scores
     def train(self, X_train, y_train):
         vocab, X_train_sparse = self._build_vocabulary(X_train)
         self._vocabulary = vocab
@@ -92,12 +98,42 @@ class NaiveBayesModel:
         return y_predictions
 
 
-
 def read_data(set_):
     return np.load(DATA_PATH / f"data_{set_}.pkl", allow_pickle=True)
 
-def preprocess(data):
-    return data
+def preprocess_line(line, analyzer, lem=True, stem=True, remove_stop_words=True):
+    # Tokenize
+    line = analyzer(line)
+
+    if lem:
+        lemmatizer = WordNetLemmatizer()
+        line = [lemmatizer.lemmatize(word)for word in line]
+
+    if stem:
+        stemmer = PorterStemmer()
+        line = [stemmer.stem(word) for word in line]
+
+    if remove_stop_words:
+        stop_words = set(stopwords.words('english'))
+        data = [word for word in line if not word in stop_words]   #TODO check that data should be data and not line
+
+    return " ".join(line)
+
+def preprocess(data, lem=True, stem=True, remove_stop_words=True, min_df=0.001):
+
+    vectorizer = CountVectorizer(min_df=min_df)
+    analyzer = vectorizer.build_analyzer()
+
+    # Preprocess line by line
+    data = [preprocess_line(line,
+                            analyzer,
+                            lem=lem,
+                            stem=stem,
+                            remove_stop_words=remove_stop_words)
+             for line in data]
+
+    # Strings to array
+    return vectorizer.fit_transform(data)
 
 def get_data(set_):
     data = read_data(set_)
@@ -111,15 +147,56 @@ def write_csv(y_prediction):
             writer.writerow([i, y])
 
 
-def main():
+def main(seed=42, lem=True, stem=True, remove_stop_words=True, min_df=0.0012):
     X_train, y_train = get_data(set_="train")
     X_test = get_data(set_="test")
 
     model = NaiveBayesModel()
-    model.train(X_train, y_train)
+
+    # pre_process features
+    X = preprocess(X_train, lem, stem, remove_stop_words, min_df)
+
+    # split train into train / val
+    X_train, X_val, y_train, y_val = train_test_split(X, y_train,
+                                                        test_size=0.2,
+                                                        random_state=seed)
+
+    val_score = model.train(X_train, y_train) # TODO model.train should take X_val and y_val and return validation scores
+
     y_prediction = model.predict(X_test)
-    write_csv(y_prediction)
+
+    return y_prediction, val_score
 
 
 if __name__ == "__main__":
-    main()
+    import nltk
+    nltk.download('wordnet')
+    nltk.download('punkt')
+    nltk.download('stopwords')
+
+    seed = 42
+
+    best_score = 0.
+    best_config = None
+    best_predictions = None
+
+    for lem in [True, False]:
+        for stem in [True, False]:
+            for remove_stop_words in [True, False]:
+                for min_df in [0, 0.0001, 0.001, 0.01, 0.1]:
+                    config = f"lem {lem}, stem {stem}, remove_stop_words {remove_stop_words}, min_df {min_df}"
+                    print(f">>> {config}")
+                    y_prediction, score = main( seed=seed,
+                                                lem=lem,
+                                                stem=stem,
+                                                remove_stop_words=remove_stop_words,
+                                                min_df=min_df)
+                    if score > best_score:
+                        best_score = score
+                        best_config = config
+                        best_predictions = y_prediction
+
+    print(f"Best score: {best_score} \n  {best_config}")
+
+    write_csv(best_predictions)
+
