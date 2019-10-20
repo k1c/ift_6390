6d7d01151184f561d118fcd2c5a04bd5b2a12dbe
k1c <cpelletier@klipfolio.com>
2019-10-20 11:46:29 -0400

    adding TFIDF

--- kaggle/solution.py
+++ kaggle/solution.py
@@ -13,12 +13,13 @@ from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
-import nltk
 from nltk.tokenize import RegexpTokenizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import pandas as pd
 import re
-from sklearn.feature_extraction.text import CountVectorizer
 
 DATA_PATH = pathlib.Path("data/")
+
 
 class BaselineModel:
     def __init__(self):
@@ -92,20 +93,35 @@ class NaiveBayesModel:
 
 
 def build_vocab_cv(X, ngram_range=(1, 1), min_df=1, max_features=None):
-    """
-    X should look like this:
-    X = [
-        'This is the first document.',
-        'This document is the second document.',
-        'And this is the third one.',
-        'Is this the first document?'
-    ]
-    """
     X = [','.join(x) for x in X]  # convert list of list of strings to list of strings
     vectorizer = CountVectorizer(ngram_range=ngram_range, min_df=min_df, max_features=max_features)
     x_train_sparse = vectorizer.fit_transform(X)
     vocab = vectorizer.vocabulary_
     return vectorizer, vocab, x_train_sparse
+
+
+def build_vocab_tfidf(X_train, documents, min_df, num_remove):
+    # compute TFIDF for each subreddit
+    TFIDF_vectorizer = TfidfVectorizer()
+    vectors = TFIDF_vectorizer.fit_transform(documents)
+    feature_names = TFIDF_vectorizer.get_feature_names()
+    dense = vectors.todense()
+    denselist = dense.tolist()
+    df = pd.DataFrame(denselist, columns=feature_names)
+
+    # TODO remove hard-coded index by=13 for conspiracy class
+    # We built a confusion matrix and saw that words in 'conspiracy' subreddit are often misclassified
+    # This may be due to the fact that these words are shared amongst other classes at a high frequency
+    # Therefore perform TFIDF and remove top-X number of words sorted by 'conspiracy' class
+    conspiracy_df = df.sort_values(by=13, axis=1, ascending=False, inplace=False)  # sorted by top conspiracy words
+    conspiracy_df.drop(conspiracy_df.columns[:num_remove], axis=1, inplace=True)
+    X_train = [','.join(x) for x in X_train]  # convert list of list of strings to list of strings
+
+    # Created vocab and sparse matrix
+    COUNT_vectorizer = CountVectorizer(vocabulary=list(conspiracy_df.columns.values), min_df=min_df)
+    x_train_sparse = COUNT_vectorizer.fit_transform(X_train)
+    vocab = COUNT_vectorizer.vocabulary_
+    return vocab, x_train_sparse
 
 
 def build_vocab(X, min_freq=None):
@@ -136,6 +152,7 @@ def build_vocab(X, min_freq=None):
 def read_data(set_):
     return np.load(DATA_PATH / f"data_{set_}.pkl", allow_pickle=True)
 
+
 def preprocess(data, lem=True, stem=True, remove_stop_words=True):
     return [preprocess_line(line, lem, stem, remove_stop_words) for line in data]
 
@@ -147,17 +164,18 @@ def preprocess_line(line, lem=True, stem=True, remove_stop_words=True):
     # lower sent
     line = line.lower()
 
-    line = line.replace('{html}', "")
+    # replace HTML
+    line = line.replace('{html}', "<HTML>")
 
     # remove whitespace
     cleanr = re.compile('<.*?>')
     line = re.sub(cleanr, '', line)
 
-    # remove any URL
-    line = re.sub(r'^https?:\/\/.*[\r\n]*', '', line)
+    # replace any URL
+    line = re.sub(r'^https?:\/\/.*[\r\n]*', '<URL>', line)
 
-    # remove nubers
-    line = re.sub('[0-9]+', '', line)
+    # remove numbers
+    # line = re.sub('[0-9]+', '', line)
 
     line = tokenizer.tokenize(line)
 
@@ -165,13 +183,13 @@ def preprocess_line(line, lem=True, stem=True, remove_stop_words=True):
         stop_words = set(stopwords.words('english'))
         line = [word for word in line if not word in stop_words]
 
-    if stem:
-        stemmer = PorterStemmer()
-        line = [stemmer.stem(word) for word in line]
-
     if lem:
         lemmatizer = WordNetLemmatizer()
         line = [lemmatizer.lemmatize(word) for word in line]
+
+    if stem:
+        stemmer = PorterStemmer()
+        line = [stemmer.stem(word) for word in line]
 
     return line
 
@@ -184,55 +202,90 @@ def write_csv(y_prediction):
             writer.writerow([i, y])
 
 
-def main(X_train, y_train, X_test, lem, stem, remove_stop_words, min_freq, alpha, grams):
+def get_text(df, label):
+    return df[df['labels'] == label]['text']
+
+
+# subreddits is a list of length 20 (20 classes)
+# each element is a string and represents all the text from a single subreddit
+def get_subreddits(df, lem, stem, remove_stop_words):
+    labels = df.labels.unique().tolist()
+    df_text_per_class = list()  # text_per_class is a list of strings
+    for label in labels:
+        df_text_per_class.append(get_text(df, label))
+
+    text_per_class = list()
+    for i in range(len(df_text_per_class)):
+        text_per_class.append(df_text_per_class[i].tolist())
+
+    # clean vocab using same params as training data
+    text_per_class = [preprocess(text_per_class[i], lem, stem, remove_stop_words) for i in range(len(text_per_class))]
+
+    subreddits = list()
+    for i in range(len(text_per_class)):
+        X = [' '.join(x) for x in text_per_class[i]]  # convert list of list of strings to list of strings
+        X = " ".join(X)
+        subreddits.append(X)
+    return subreddits
+
+
+def main(is_train, score, X_train, y_train, X_test, lem, stem, remove_stop_words, min_df, alpha, num_remove):
+    # build pandas dataframe to gather text from subreddits together
+    df_X = pd.DataFrame(X_train, columns=['text'])
+    df_Y = pd.DataFrame(y_train, columns=['labels'])
+    df = pd.concat([df_X, df_Y], axis=1)
+
     X_train = preprocess(X_train, lem=lem, stem=stem, remove_stop_words=remove_stop_words)
     X_test = preprocess(X_test, lem=lem, stem=stem, remove_stop_words=remove_stop_words)
 
-    # split train into train / val
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+    if is_train:
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
-    # vocab, X_train_sparse = build_vocab(X_train, min_freq=min_freq)
-    vocab, X_train_sparse = build_vocab(X_train, min_freq=min_freq)
+    subreddits = get_subreddits(df, lem, stem, remove_stop_words)
+
+    vocab, X_train_sparse = build_vocab_tfidf(X_train, subreddits, min_df, num_remove)
 
     model = NaiveBayesModel(vocab=vocab, alpha=alpha)
     model.train(X_train_sparse, y_train)
-    score = model.get_accuracy(X_val, y_val)
+    if is_train:
+        score = model.get_accuracy(X_val, y_val)
 
-    # X_test = [','.join(x) for x in X_test] # convert list of list of strings to list of strings
-    # test_x_sparse = vectorizer.transform(X_test)
     y_prediction = model.predict(X_test)
     return y_prediction, score
 
 
 if __name__ == "__main__":
     import nltk
-    # nltk.download('wordnet')
-    ##nltk.download('stopwords')
+
+    nltk.download('wordnet')
+    nltk.download('stopwords')
+    nltk.download('punkt')
 
     X_train, y_train = read_data(set_="train")
     X_test = read_data(set_="test")
 
+    is_train = True
+
     best_score = 0.
+    score = 0.
     best_config = None
     best_predictions = None
-
-    for lem in [False]:  # [True, False]
-        for stem in [False]:  # [True, False]
-            for remove_stop_words in [True]:  # [True, False]
-                for min_freq in [0, 1, 5, 10]:  # [0,1]
-                    for alpha in [0.01, 0.1, 0.5, 1]:
-                        for grams in [(1, 1)]: # [(1,1),(1,2)]
-                            config = f"min_freq {min_freq}, smoothing_param {alpha}, lem {lem}, stem {stem}, remove_stop_word {remove_stop_words}, Ngrams {grams}"
+    for lem in [True, False]:  # HP_Search params: [True, False]
+        for stem in [True, False]:  # HP_Search params: [True, False]
+            for remove_stop_words in [True, False]:  # HP_Search params: [True, False]
+                for min_df in [0, 1]:  # HP_Search params: [0, 1]
+                    for alpha in [0.01, 0.05, 0.1, 0.15, 0.25, 0.5]:  # HP_Search params: [0.01, 0.05, 0.1, 0.15, 0.25, 0.5]
+                        for num_remove in [0, 15, 45, 50, 150, 500]:  # HP_Search params: [0, 15, 45, 50, 150, 500]
+                            config = f"min_df {min_df}, smoothing_param {alpha}, lem {lem}, stem {stem}, remove_stop_word {remove_stop_words}, num_remove {num_remove}"
                             print(f">>> {config}")
-                            y_prediction, score = main(X_train, y_train, X_test, lem, stem, remove_stop_words, min_freq,
-                                                       alpha, grams)
+                            y_prediction, score = main(is_train, score, X_train, y_train, X_test, lem, stem,
+                                                       remove_stop_words, min_df, alpha, num_remove)
                             print("SCORE", score)
                             if score > best_score:
                                 best_score = score
+                                print("BEST SCORE", best_score)
                                 best_config = config
                                 best_predictions = y_prediction
-
-    print(f"Best score: {best_score} \n  Best config: {best_config}")
-
-    write_csv(best_predictions)
+    if not is_train:
+        write_csv(best_predictions)
 
