93f65ae0ed95c1dce0bf211e10a71bbdb05e1ee8
Isabelle Bouchard <isabelle@elementai.com>
2019-11-10 14:43:17 -0500

    flair embeddings

--- kaggle/scikit_classifiers.py
+++ kaggle/scikit_classifiers.py
@@ -13,6 +13,10 @@ from nltk.stem import PorterStemmer
 from sklearn.model_selection import train_test_split
 from nltk.tokenize import RegexpTokenizer
 import array
+from flair.data import Sentence
+from flair.embeddings import DocumentPoolEmbeddings
+from flair.embeddings import FlairEmbeddings
+from flair.embeddings import WordEmbeddings
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -26,70 +30,56 @@ from sklearn.svm import SVC
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import VotingClassifier
 
 DATA_PATH = pathlib.Path("data/")
 
 _CLASSIFIERS = {
     "Random Forest": (
-        RandomForestClassifier(n_estimators=200, random_state=42),
+        RandomForestClassifier(n_estimators=200, max_depth=125, random_state=42),
         {
-            'vect__min_df': (2, 10, 20),
-            'vect__max_df': (100, 250, 500),
-            'clf__max_depth': (50, 100, 125)
+            "clf__max_depth": [200, 300]
         }
     ),
     "Logistic Regression": (
-        LogisticRegression(solver="saga", multi_class="multinomial"),
+        LogisticRegression(solver="saga", multi_class="multinomial", penalty="l2"),
         {
-            "clf__penalty": ["l1", "l2"],
-            'vect__min_df': (2, 10, 20),
-            'vect__max_df': (100, 250, 500),
 
         }
     ),
     "Naive Bayes": (
         MultinomialNB(),
         {
-            "clf__alpha": [0., 0.25, 0.5, 0.75, 1.],
-            'vect__min_df': (2, 10, 20),
-            'vect__max_df': (100, 250, 500),
-
+            "clf__alpha": [0.25, 0.5],
         }
     ),
     "SVM_0": (
         LinearSVC(),
         {
-            'vect__min_df': (2, 10, 20),
-            'vect__max_df': (100, 250, 500),
-        },
-    ),
-    "SVM_1": (
-        SVC(),
-        {
-            'vect__min_df': (2, 10, 20),
-            'vect__max_df': (100, 250, 500),
         },
     ),
     "SVM_2": (
-        SGDClassifier(loss='hinge', random_state=42),
+        SGDClassifier(loss='hinge', alpha=0.001, random_state=42),
         {
-            'vect__min_df': (2, 10, 20),
-            'vect__max_df': (100, 250, 500),
-            'clf__alpha': (1e-2, 1e-3),
         }
     ),
 }
 
+_EMBEDDER = DocumentPoolEmbeddings([
+    FlairEmbeddings("en-forward"),
+    FlairEmbeddings("en-backward"),
+    WordEmbeddings("glove"),
+], "mean")
 
 def read_data(set_):
     return np.load(DATA_PATH / f"data_{set_}.pkl", allow_pickle=True)
 
 
-def preprocess(data, lem=True, stem=True):
-    return [preprocess_line(line, lem, stem) for line in data]
+def preprocess(data, lem=True, stem=True, embed=True):
+    return [preprocess_line(line, lem, stem, embed) for line in data]
 
 
-def preprocess_line(line, lem=True, stem=True):
+def preprocess_line(line, lem=True, stem=True, embed=True):
     tokenizer = RegexpTokenizer(r"(?u)\b\w\w+\b")
 
     # lower sent
@@ -105,7 +95,14 @@ def preprocess_line(line, lem=True, stem=True):
         stemmer = PorterStemmer()
         line = [stemmer.stem(word) for word in line]
 
-    return " ".join(line)
+    line = " ".join(line)
+
+    if embed:
+        sentence = Sentence(line)
+        _EMBEDDER.embed(sentence)
+        line = sentence.get_embedding().detach().numpy()
+
+    return line
 
 
 def write_csv(y_prediction):
@@ -116,12 +113,15 @@ def write_csv(y_prediction):
             writer.writerow([i, y])
 
 
-def run_grid_search(clf, parameters, X_train, X_val, y_train, y_val):
-    text_clf = Pipeline([
-        ('vect', CountVectorizer(stop_words='english')),
-        ('tfidf', TfidfTransformer()),
-        ('clf', clf),
-    ])
+def run_grid_search(clf, parameters, X_train, X_val, y_train, y_val, embed=False):
+    if embed:
+        text_clf = clf
+    else:
+        text_clf = Pipeline([
+            ('vect', CountVectorizer(stop_words='english', min_df=2, max_df=500)),
+            ('tfidf', TfidfTransformer()),
+            ('clf', clf),
+        ])
 
     gs_clf = GridSearchCV(text_clf, parameters, cv=5, iid=False, n_jobs=-1)
 
@@ -133,8 +133,11 @@ def run_grid_search(clf, parameters, X_train, X_val, y_train, y_val):
     return accuracy, gs_clf.best_params_
 
 
-def main(X_train, X_val, y_train, y_val, clf_name="SVM_1", bagging=False, boosting=False):
-    clf, params = _CLASSIFIERS[clf_name]
+def main(X_train, X_val, y_train, y_val, clf_name="SVM_1",
+         bagging=False, boosting=False, voting=False, embed=False):
+
+    if isinstance(clf_name, str):
+        clf, params = _CLASSIFIERS[clf_name]
 
     if clf_name == "RandomForest" and (boosting or bagging):
         raise ValueError(f"no boost or bagg for {clf_name}")
@@ -150,9 +153,15 @@ def main(X_train, X_val, y_train, y_val, clf_name="SVM_1", bagging=False, boosti
         clf_name += " boost"
         params = {}
 
+    if voting:
+        estimators = [(name, _CLASSIFIERS[name][0]) for name in clf_name]
+        clf = VotingClassifier(estimators=estimators, voting='hard')
+        clf_name = "voting"
+        params= {}
+
 
     print(f"Running GS for {clf_name}...")
-    accuracy, best_params = run_grid_search(clf, params, X_train, X_val, y_train, y_val)
+    accuracy, best_params = run_grid_search(clf, params, X_train, X_val, y_train, y_val, embed)
     print(f">>> {clf_name} score = {accuracy}")
     print(f"{best_params}")
     return accuracy, best_params
@@ -166,23 +175,19 @@ if __name__ == "__main__":
     # Read data
     X_raw, y_raw = read_data(set_="train")
 
-    for lem in [True, False]:
+    for lem in [False]:
         print(f"LEM {lem}")
-        for stem in [True, False]:
-            print(f"STEM {lem}")
+        for stem in [False]:
+            print(f"STEM {stem}")
             # Preprocess data
-            X = preprocess(X_raw, lem=lem, stem=stem)
+            X = preprocess(X_raw, lem=lem, stem=stem, embed=True)
             X_train, X_val, y_train, y_val = train_test_split(X,
                                                               y_raw,
                                                               test_size=0.2,
                                                               random_state=42)
-            for model in _CLASSIFIERS.keys():
-                main(X_train, X_val, y_train, y_val, model)
 
 
-# TODO:
-# - Voting classifier
-#   >>> from sklearn.ensemble import VotingClassifier
-#   >>> model1 = LogisticRegression(random_state=1)
-#   >>> model2 = tree.DecisionTreeClassifier(random_state=1)
-#   >>> model = VotingClassifier(estimators=[('lr', model1), ('dt', model2)], voting='hard')
+
+#            for model in _CLASSIFIERS.keys():
+            main(X_train, X_val, y_train, y_val, clf_name="Naive Bayes", embed=True)
+
